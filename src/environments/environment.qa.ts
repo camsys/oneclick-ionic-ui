e06c595bb5da40feb434942c2f918608417fc1a2
tmahtab <tmahtab@camsys.com>
2020-07-31 10:53:47 -0700

    Updated qa environment for 211Ride

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,11 +1,11 @@
-export const environment = {
-  BASE_ONECLICK_URL: 'http://occ-lynx-qa.herokuapp.com/api/v2/',
-  AWS_LOCALE_URL: 'http://occ-lynx-qa.herokuapp.com/',
-  AWS_IMAGE_ASSET_BUCKET: 'https://s3.us-east-1.amazonaws.com/occ-lynx-qa/assets/img/',
+export const environment = {  
+  BASE_ONECLICK_URL: 'http://occ-ieuw-qa.herokuapp.com/api/v2/',
+  AWS_LOCALE_URL: 'http://occ-ieuw-qa.herokuapp.com/',
+  AWS_IMAGE_ASSET_BUCKET: 'https://s3.us-west-1.amazonaws.com/ui-ieuw-qa/assets/img/',
   DEFAULT_LOCATION: {
     lat: 34.302587,
     lng: -119.318995
   },
-  AVAILABLE_LOCALES: [ 'en', 'es', 'pt', 'vi', 'ht'],
+  AVAILABLE_LOCALES: [ 'en', 'es', 'pt', 'vi', 'ht', 'zh', 'fa'],
   DEFAULT_LOCALE: 'en'
 };
