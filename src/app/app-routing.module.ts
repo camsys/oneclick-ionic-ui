3c1ca69050a29b03d7891d6cc5695432b5e63144
rbrown <rbrown@camsys.com>
2022-10-08 13:57:46 -0400

    updated route paths and app module; removed unused home.page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,145 +1,125 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NoPreloading, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'categories-for211',
+    path: 'categories',
     loadChildren: () => import('./pages/211/categories-for211/categories-for211.module').then( m => m.CategoriesFor211PageModule)
   },
   {
-    path: 'service-for211-detail',
+    path: 'service_detail',
     loadChildren: () => import('./pages/211/service-for211-detail/service-for211-detail.module').then( m => m.ServiceFor211DetailPageModule)
   },
   {
-    path: 'service-for211-modal',
-    loadChildren: () => import('./pages/211/service-for211-modal/service-for211-modal.module').then( m => m.ServiceFor211ModalPageModule)
+    path: 'service_detail/:service_id/:location_id',
+    loadChildren: () => import('./pages/211/service-for211-detail/service-for211-detail.module').then( m => m.ServiceFor211DetailPageModule)
   },
   {
-    path: 'services',
+    path: 'services/:code',
     loadChildren: () => import('./pages/211/services/services.module').then( m => m.ServicesPageModule)
   },
   {
-    path: 'services-list-tab',
-    loadChildren: () => import('./pages/211/services-list-tab/services-list-tab.module').then( m => m.ServicesListTabPageModule)
-  },
-  {
-    path: 'services-map-tab',
-    loadChildren: () => import('./pages/211/services-map-tab/services-map-tab.module').then( m => m.ServicesMapTabPageModule)
-  },
-  {
-    path: 'sub-subcategories-for211',
+    path: 'sub_sub_categories/:code',
     loadChildren: () => import('./pages/211/sub-subcategories-for211/sub-subcategories-for211.module').then( m => m.SubSubcategoriesFor211PageModule)
   },
   {
-    path: 'subcategories-for211',
+    path: 'sub_categories',
     loadChildren: () => import('./pages/211/subcategories-for211/subcategories-for211.module').then( m => m.SubcategoriesFor211PageModule)
   },
   {
-    path: 'about-us',
+    path: 'sub_categories/:code',
+    loadChildren: () => import('./pages/211/subcategories-for211/subcategories-for211.module').then( m => m.SubcategoriesFor211PageModule)
+  },
+  {
+    path: 'about_us',
     loadChildren: () => import('./pages/about-us/about-us.module').then( m => m.AboutUsPageModule)
   },
   {
-    path: 'contact-us',
+    path: 'contact_us',
     loadChildren: () => import('./pages/contact-us/contact-us.module').then( m => m.ContactUsPageModule)
   },
   {
-    path: 'directions',
+    path: 'trip_directions/:trip_id',
     loadChildren: () => import('./pages/directions/directions.module').then( m => m.DirectionsPageModule)
   },
   {
-    path: 'directions-map-tab',
-    loadChildren: () => import('./pages/directions-map-tab/directions-map-tab.module').then( m => m.DirectionsMapTabPageModule)
-  },
-  {
-    path: 'directions-steps-tab',
-    loadChildren: () => import('./pages/directions-steps-tab/directions-steps-tab.module').then( m => m.DirectionsStepsTabPageModule)
-  },
-  {
-    path: 'email-itinerary-modal',
-    loadChildren: () => import('./pages/email-itinerary-modal/email-itinerary-modal.module').then( m => m.EmailItineraryModalPageModule)
-  },
-  {
-    path: 'email-modal',
-    loadChildren: () => import('./pages/email-modal/email-modal.module').then( m => m.EmailModalPageModule)
-  },
-  {
-    path: 'feedback-modal',
-    loadChildren: () => import('./pages/feedback-modal/feedback-modal.module').then( m => m.FeedbackModalPageModule)
-  },
-  {
-    path: 'feedback-status',
+    path: 'feedback_status',
     loadChildren: () => import('./pages/feedback-status/feedback-status.module').then( m => m.FeedbackStatusPageModule)
   },
   {
-    path: 'help-me-find',
+    path: 'home',
     loadChildren: () => import('./pages/help-me-find/help-me-find.module').then( m => m.HelpMeFindPageModule)
   },
   {
-    path: 'language-selector-modal',
-    loadChildren: () => import('./pages/language-selector-modal/language-selector-modal.module').then( m => m.LanguageSelectorModalPageModule)
-  },
-  {
-    path: 'paratransit-services',
+    path: 'paratransit_services',
     loadChildren: () => import('./pages/paratransit-services/paratransit-services.module').then( m => m.ParatransitServicesPageModule)
   },
   {
-    path: 'resend-email-confirmation',
+    path: 'paratransit_services/:trip_id',
+    loadChildren: () => import('./pages/paratransit-services/paratransit-services.module').then( m => m.ParatransitServicesPageModule)
+  },
+  {
+    path: 'resend_email_confirmation',
     loadChildren: () => import('./pages/resend-email-confirmation/resend-email-confirmation.module').then( m => m.ResendEmailConfirmationPageModule)
   },
   {
-    path: 'reset-password',
+    path: 'reset_password',
     loadChildren: () => import('./pages/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
   },
   {
-    path: 'sign-in',
+    path: 'sign_in',
     loadChildren: () => import('./pages/sign-in/sign-in.module').then( m => m.SignInPageModule)
   },
   {
-    path: 'sign-up',
+    path: 'sign_up',
     loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
   {
-    path: 'sms-modal',
-    loadChildren: () => import('./pages/sms-modal/sms-modal.module').then( m => m.SmsModalPageModule)
-  },
-  {
-    path: 'taxi-services',
+    path: 'taxi_services/:trip_id',
     loadChildren: () => import('./pages/taxi-services/taxi-services.module').then( m => m.TaxiServicesPageModule)
   },
   {
-    path: 'transportation-eligibility',
+    path: 'transportation_eligibility/:trip_id',
     loadChildren: () => import('./pages/transportation-eligibility/transportation-eligibility.module').then( m => m.TransportationEligibilityPageModule)
   },
   {
-    path: 'trip-response',
+    path: 'trip_options',
     loadChildren: () => import('./pages/trip-response/trip-response.module').then( m => m.TripResponsePageModule)
   },
   {
-    path: 'unsubscribe',
+    path: 'trip_options/:trip_id',
+    loadChildren: () => import('./pages/trip-response/trip-response.module').then( m => m.TripResponsePageModule)
+  },
+  {
+    path: 'trip_options/:trip_id/:service_id/:location_id',
+    loadChildren: () => import('./pages/trip-response/trip-response.module').then( m => m.TripResponsePageModule)
+  },
+  {
+    path: 'trip_options/:trip_id/:trip_id/:service_id/:location_id',
+    loadChildren: () => import('./pages/trip-response/trip-response.module').then( m => m.TripResponsePageModule)
+  },
+  {
+    path: 'unsubscribe/:email',
     loadChildren: () => import('./pages/unsubscribe/unsubscribe.module').then( m => m.UnsubscribePageModule)
   },
   {
-    path: 'user-locator',
+    path: 'locator/:viewType',
     loadChildren: () => import('./pages/user-locator/user-locator.module').then( m => m.UserLocatorPageModule)
   },
   {
-    path: 'user-profile',
+    path: 'profile',
     loadChildren: () => import('./pages/user-profile/user-profile.module').then( m => m.UserProfilePageModule)
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading })
   ],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AnimationController, IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,9 +19,28 @@ import { ScheduleDayPipe } from './pipes/schedule-day.pipe';
 import { ScheduleTimePipe } from './pipes/schedule-time.pipe';
 import { ToStringPipe } from './pipes/to-string.pipe';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "/assets/i18n/", ".json");
+}
+
 @NgModule({
   declarations: [AppComponent, FormatPhoneNumberPipe, PrettyDistancePipe, PrettyFarePipe, PrettyTableNamePipe, PrettyTimePipe, ScheduleDayPipe, ScheduleTimePipe, ToStringPipe],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }), 
+    IonicModule.forRoot({
+      swipeBackEnabled: false,
+      navAnimation: _ => new AnimationController().create()//turned off default page transition animation
+    }), ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
