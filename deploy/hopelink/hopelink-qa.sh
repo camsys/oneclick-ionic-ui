fdb38eced9fa6b9a5ea8a5736f0de67ff8a59df9
rbrown <rbrown@camsys.com>
2023-01-04 13:43:56 -0500

    added correct API url and correct S3 bucket URL for Hopelink QA instance

--- deploy/hopelink/hopelink-qa.sh
+++ deploy/hopelink/hopelink-qa.sh
@@ -6,7 +6,7 @@ client=hopelink
 iconimagename=hopelink_icon.png
 backgroundimagename=hopelink_background.png
 
-s3bucket=s3://UNKNOWN
+s3bucket=s3://ui-hopelink-qa
 awsprofile=hope
 prodflag=--prod
 
