5a44d9ccd490384b3c4f38db986d429860370e71
rbrown <rbrown@camsys.com>
2023-07-14 12:33:39 -0400

    updated deployment files for 211Ride to reflect the new all aws qa environment

--- deploy/211ride/211ride-qa.sh
+++ deploy/211ride/211ride-qa.sh
@@ -6,7 +6,8 @@ client=211ride
 iconimagename=211-ride@1x.png
 backgroundimagename=GettyImages-930896740_sized.png
 
-s3bucket=s3://ui-ieuw-qa
+#s3bucket=s3://ui-ieuw-qa
+s3bucket=s3://ui-211ride-qa
 awsprofile=ieuw
 prodflag=--prod
 
