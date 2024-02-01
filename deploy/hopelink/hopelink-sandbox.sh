29e2025ca71749ee57d35db580ed5b65ad7c6aff
rbrown <rbrown@camsys.com>
2024-02-01 11:46:03 -0500

    added hopelink sandbox configuration files

---
+++ deploy/hopelink/hopelink-sandbox.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+environment=sandbox
+
+client=hopelink
+iconimagename=hopelink_icon.png
+backgroundimagename=hopelink_background.png
+
+s3bucket=s3://ui-hopelink-sandbox
+awsprofile=hope
+prodflag=--prod
+
+source ../common.sh
