08e190237d4c0b33d91e5abd419aa60a43fd6bc9
rbrown <rbrown@camsys.com>
2024-02-20 14:39:53 -0500

    updated version to 1.1.0

--- deploy/211ride/environments/environment-211ride.dev.ts
+++ deploy/211ride/environments/environment-211ride.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   
   //BASE_ONECLICK_URL: 'http://occ-lynx-dev.herokuapp.com/api/v2/'
-  BASE_ONECLICK_URL: 'https://occ-ieuw-qa.herokuapp.com/api/v2/'
+  BASE_ONECLICK_URL: 'http://occ-ieuw-dev-bebb859ea6be.herokuapp.com/api/v2/'
   
 
 };
