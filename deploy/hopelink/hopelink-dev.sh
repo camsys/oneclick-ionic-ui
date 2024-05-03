aa3084613df6e7c90572d837570c6bac3792d837
rbrown <rbrown@camsys.com>
2024-05-03 13:33:56 -0400

    OCC-1202 adding configuration for inserting Google API keys that are client and environment specific

--- deploy/hopelink/hopelink-dev.sh
+++ deploy/hopelink/hopelink-dev.sh
@@ -10,4 +10,8 @@ s3bucket=s3://ui-hopelink-dev
 awsprofile=hope
 prodflag=--prod
 
+#copy index.html.tmpl to replace google api key
+sed 's/GOOGLE_API_KEY/AIzaSyAELZ3K_3wIKhJwkwRjt8l1aCxGOFIQdhY/' ../index.html.tmpl > ../../src/index.html
+
+
 source ../common.sh
--- deploy/hopelink/hopelink-local.sh
+++ deploy/hopelink/hopelink-local.sh
@@ -5,4 +5,8 @@ iconimagename=hopelink_icon.png
 backgroundimagename=hopelink_background.png
 environment=local
 
+#copy index.html.tmpl to replace google api key
+sed 's/GOOGLE_API_KEY/AIzaSyAELZ3K_3wIKhJwkwRjt8l1aCxGOFIQdhY/' ../index.html.tmpl > ../../src/index.html
+
+
 source ../local-common.sh
