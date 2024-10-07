6bc97274ade26ca8c6d03fe7b7b8ab065824fc02
rbrown <rbrown@camsys.com>
2024-10-07 12:56:25 -0400

    switched deploy script to use cp instead of sync aws command because sync was stalling for a really long time

--- deploy/common.sh
+++ deploy/common.sh
@@ -38,7 +38,7 @@ cp ./variables-$client.scss ../../src/theme/variables.scss
 
 # build code (with or without --prod flag) and upload to aws
 ionic build $prodflag
-aws s3 sync ../../www/ $s3bucket --acl public-read --cache-control no-cache --profile $awsprofile
+aws s3 cp --recursive ../../www/ $s3bucket --acl public-read --cache-control no-cache --profile $awsprofile
 
 mv ../../src/environments/appConfig.ts.bak ../../src/environments/appConfig.ts
 
