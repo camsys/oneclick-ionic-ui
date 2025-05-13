570498ccf85e8cf0986d4085a8f7d89f5bfe6faf
rbrown <rbrown@camsys.com>
2025-05-13 14:48:09 -0400

    new deployment scripts

--- deploy/example-client/example-client-dev.sh
+++ deploy/example-client/example-client-dev.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-environment=prod
+environment=dev
 
-client=hopelink
-iconimagename=hopelink_icon.png
-backgroundimagename=hopelink_background.png
+client=example-client
+iconimagename=example-client_icon.png
+backgroundimagename=example-client_background.png
 src_deploy_dir=**FULL-PATH-TO**/oneclick-ionic-ui/deploy
 
-s3bucket=s3://ui-hopelink-prod
-awsprofile=hope
+s3bucket=s3://ui-example-client-dev
+awsprofile=example-client-profile
 prodflag=--prod
 
 #copy index.html.tmpl to replace google api key
-#using restricted key!
-sed 's/GOOGLE_API_KEY/AIzaSyD-IDRbL8bFdNHkSlU1hOCGepPaHXP-hRc/' ../index.html.tmpl > ../../src/index.html
 sed 's/GOOGLE_API_KEY/{{REPLACE_WITH_KEY}}/' $src_deploy_dir/index.html.tmpl > $src_deploy_dir/../src/index.html
+
 
 source $src_deploy_dir/common.sh
