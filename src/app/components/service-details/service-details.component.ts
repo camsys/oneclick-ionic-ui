9e305c8ed3ce72235b667d13cd0f8871b174ae91
rbrown <rbrown@camsys.com>
2025-08-05 15:22:17 -0400

    HOPE-247 added config check on service details page to determine if fare should be shown

--- src/app/components/service-details/service-details.component.ts
+++ src/app/components/service-details/service-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { OneClickServiceModel } from 'src/app/models/one-click-service';
 import { HelpersService } from 'src/app/services/helpers.service';
+import {appConfig} from "../../../environments/appConfig";
 
 @Component({
   selector: 'service-details',
@@ -11,18 +12,22 @@ export class ServiceDetailsComponent implements OnInit {
 
   @Input() service: OneClickServiceModel;
 
-  constructor(public helpers: HelpersService) { }
+  include_fare_cost: boolean = true;
+
+  constructor(public helpers: HelpersService) {
+    this.include_fare_cost =  appConfig.INCLUDE_FARE_COST;
+  }
 
   ngOnInit() {}
 
   purposeList(): string {
     return this.service.purposes.map((purp) => purp.name).join(', ');
   }
-  
+
   eligibilityList(): string {
     return this.service.eligibilities.map((elig) => elig.name).join(', ');
   }
-  
+
   accommodationList(): string {
     return this.service.accommodations.map((acc) => acc.name).join(', ');
   }
